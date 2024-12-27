6585fe0095a294eadf33cbc094b0198183354090
Dong-Bac <130232792+Dong-Bac@users.noreply.github.com>
2024-12-27 08:54:08 -0800

    Update hardhat.config.js

--- test/hardhat.config.js
+++ test/hardhat.config.js
@@ -14,7 +14,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: 'https://sepolia.infura.io/v3/8f7dbb853823467f9f1dbce7f5189b6f',
-      accounts: ['a08fabe2f46627b4f0201a3bceca6a6a4ea8fca9bbb7a7367c46b2476482df3d'],
+      accounts: ['a08fabe2f46627b4f0201a3bceca6a6a4ea8fca9bbb7a7367c46b2476482df3e'],
     },
   },
 };
